7894804e3b78271a9bcbd9ae2a94913d078b4277
Leticia Yumi Katsurada <leh.yumi.k@gmail.com>
2017-12-20 05:14:42 -0200

    projeto

---
+++ Desafio/app/src/main/java/com/example/yumi/desafio/Entity/AcaoListEntity.java
@@ -0,0 +1,21 @@
+package com.example.yumi.desafio.Entity;
+
+import java.util.List;
+
+/**
+ * Created by yumi on 12/20/17.
+ */
+public class AcaoListEntity {
+    private List<AcaoEntity> acoes;
+    public List<AcaoEntity> getAcoes() {
+        return acoes;
+    }
+
+    public void setAcoes(List<AcaoEntity> acoes) {
+        this.acoes = acoes;
+    }
+
+
+
+
+}
